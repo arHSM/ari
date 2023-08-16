3dcb55e1fa776dff32e8802cc40980b106e9cfa5
dolfies <jeyalfie47@gmail.com>
2023-08-16 14:01:31 +0300

    Make command guild-only and add error handling

--- ari/cogs/shell.py
+++ ari/cogs/shell.py
@@ -1,15 +1,23 @@
-from discord.ext.commands import Cog, Context, command
+from discord.ext import commands
 
 from ..helpers.command_paginator import CommandPaginatorView
 from ..helpers.shell import ShellExecutor
 
 
-class Shell(Cog):
-    def __init__(self) -> None:
-        return
+class Shell(commands.Cog):
+    async def cog_command_error(self, ctx: commands.Context, error: Exception) -> None:
+        error = getattr(error, "original", error)
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send("This command cannot be used in DMs.")
+        elif isinstance(error, commands.PrivateMessageOnly):
+            await ctx.send("This command can only be used in DMs.")
+        else:
+            await ctx.send(f"An unknown error occurred: {error}")
+            raise error
 
-    @command(name="sh", aliases=["shell", "bash", "exec", "eval"])
-    async def shell(self, ctx: Context, *, cmd: str):
+    @commands.guild_only()
+    @commands.command(name="sh", aliases=["shell", "bash", "exec", "eval"])
+    async def shell(self, ctx: commands.Context, *, cmd: str):
         view = CommandPaginatorView(ctx)
 
         await view.reply()
